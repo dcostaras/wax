f5aea8b6836c15db723029adbbcb48e22792132a
Donavan-Ross Costaras <donavan@Donavan-Rosss-MacBook-Air.local>
2021-01-17 22:00:28 +0000

    Add example document structure

--- src/dev/donavan/wax.clj
+++ src/dev/donavan/wax.clj
@@ -1,2 +1,17 @@
 (ns dev.donavan.wax
   (:require [com.rpl.specter :as sp]))
+
+(def document-example-2
+  {:length 7
+   :type :document
+   :selection [1 1]
+   :script
+   [{:word "foo"
+     :type :word
+     :length 3}
+    {:word " "
+     :type :word
+     :length 1}
+    {:word "bar"
+     :type :word
+     :length 3}]})
